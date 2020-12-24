4bf247694107a6cef9a5f4de0fe651d1ec9bd620
Jonathan Hernández <aggressivepixels@gmail.com>
2020-12-24 13:21:08 -0400

    Listing 1

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+const IMAGE_WIDTH: usize = 256;
+const IMAGE_HEIGHT: usize = 256;
+
+fn main() {
+    println!("P3");
+    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
+    println!("255");
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            let r = i as f64 / (IMAGE_WIDTH as f64 - 1.0);
+            let g = j as f64 / (IMAGE_HEIGHT as f64 - 1.0);
+            let b = 0.25;
+
+            println!(
+                "{} {} {}",
+                (r * 255.999) as usize,
+                (g * 255.999) as usize,
+                (b * 255.999) as usize
+            )
+        }
+    }
+}
